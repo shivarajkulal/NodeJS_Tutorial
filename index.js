7979e32f866cc6d0fb0ba89c38087e0c774c9334
shivarajkulal <shivarajkulalsn884@gmail.com>
2024-02-23 18:41:51 +0530

    Middlewares handling

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.listen(5000);
 tutorial 23: template engine
 ************
 
- */
+ 
 app.set("view engine", "ejs");
 app.get("/profile", (req, res) => {
   const user ={
@@ -77,11 +77,75 @@ app.get("/profile", (req, res) => {
 
 });
 
-app.set("view engine", "ejs");
 app.get("", (req, res) => {
   res.render("index");
+});
+
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 app.listen(5000,()=>{
  console.log("listening to port 5000");
 })
+*/
+
+/*
+tutorial 25: expressJS Middleware
+**********************************
+const express = require('express');
+const app = express();
+const reqFilter = (req, resp, next) => {
+    if (!req.query.age) {
+        resp.send("Please provide your age")
+    }
+    else if (req.query.age<18) {
+        resp.send("You are under aged")
+    }
+    else {
+        next();
+    }
+}
+
+app.use(reqFilter);
+
+app.get('/', (res, resp) => {
+    resp.send('Welcome to Home page')
+});
+
+app.get('/users', (res, resp) => {
+    resp.send('Welcome to Users page')
+});
+app.listen(5000)
+*/
+/*
+tutorial 26:Middlewre level
+****************************
+const express = require('express');
+const reqFilter= require('./middleware');
+const app = express();
+const route= express.Router();
+
+
+// app.use(reqFilter);
+route.use(reqFilter)
+app.get('/', (res, resp) => {
+    resp.send('Welcome to Home page')
+});
+
+app.get('/users', (res, resp) => {
+    resp.send('Welcome to Users page')
+});
+
+route.get('/about', (res, resp) => {
+    resp.send('Welcome to About page')
+});
+route.get('/contact', (res, resp) => {
+    resp.send('Welcome to contact page')
+});
+
+app.use('/',route);
+
+app.listen(5000)
+*/
