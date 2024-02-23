503a543ccc6ce4a801068f781191975a080bb4dd
shivarajkulal <shivarajkulalsn884@gmail.com>
2024-02-23 09:35:56 +0530

    Initial Commit

---
+++ tutorial_5/app.js
@@ -0,0 +1,20 @@
+module.exports = {
+    x :10,
+    y : 20,
+    sum:function(x,y){
+        
+        return x + y ;
+    }
+}
+
+/*// Assigning to exports will not modify module, must use module.exports
+module.exports = class Square {
+  constructor(width) {
+    this.width = width;
+  }
+
+  area() {
+    return this.width ** 3;
+  }
+};
+*/
---
+++ tutorial_5/index.js
@@ -0,0 +1,15 @@
+const app = require("./app.js");
+console.log(`sum function call: ${app.sum()}`);
+console.log(`call for x variable: ${app.x}`)
+console.log(`call for x variable: ${app.y}`);
+
+/*
+// Importing the cube module from app.js
+const cube = require("./app.js");
+
+// Creating a new instance of the cube with side length 2
+const mySquare = new cube(2);
+
+// Logging the area of mySquare to the console
+console.log(`The area of mySquare is ${mySquare.area()}`);
+*/
