46fdf6f53a87ec8d077c16faaac5f48725c9ae4a
shivarajkulal <shivarajkulalsn884@gmail.com>
2024-02-23 10:57:30 +0530

    NodeJS Basic

---
+++ tutorial_6/calc_call.js
@@ -0,0 +1,25 @@
+
+//local modules 
+
+const calculator = require("./calc");
+
+let x = 50,
+  y = 10;
+
+console.log("Addition of 50 and 10 is " + calculator.add(x, y));
+
+console.log("Subtraction of 50 and 10 is " + calculator.sub(x, y));
+
+console.log("Multiplication of 50 and 10 is " + calculator.mult(x, y));
+
+console.log("Division of 50 and 10 is " + calculator.div(x, y));
+
+/*
+Third party modules:
+
+Examples:
+npm install express
+npm install mongoose
+
+*/
+//References: https://www.geeksforgeeks.org/node-js-modules/
